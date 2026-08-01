06313979f769217d795612cab087419419792c62
Josehugo <josehugo.tec@gmail.com>
2026-08-01 08:51:04 -0500

    actualizacion 44.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='buscador-materiales-v44-2';
+const CACHE='buscador-materiales-v44-4';
 const ASSETS=['./','./index.html','./data.js','./preloaded-stock.js','./manifest.webmanifest','./favicon-16.png','./favicon-32.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
