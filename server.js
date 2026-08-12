9dfa3800dc92a857ec8e98b9a8f4adf1c0c6fefe
Tualiadoenusa <290141572+IDMMV@users.noreply.github.com>
2026-08-12 15:16:30 -0500

    VERSION FINAL

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
+app.get('/api/config', (req, res) => {
+  res.json({
+    scriptUrl: process.env.GOOGLE_APPS_SCRIPT_URL || '',
+    scriptToken: process.env.GOOGLE_APPS_SCRIPT_TOKEN || ''
+  });
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
